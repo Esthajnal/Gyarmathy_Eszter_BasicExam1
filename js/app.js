119033326a5f43eb048755763192adec4d40dc60
Gyarmathy Eszter <tucs2010@gmail.com>
2018-06-11 16:41:00 +0200

    statistics functions

--- js/app.js
+++ js/app.js
@@ -24,7 +24,7 @@ function successAjax(xhttp) {
   for (var i = 0; i < userDatas.length; i++) {
     cartes(userDatas[i], objShapceshipList);
   }
-  console.log(everyWillBeUnknown(userDatas));
+  objStatisticsDiv.innerHTML = `${oneCrew(userDatas)},`
 
 }
 //ár szerint növekvő function
@@ -59,6 +59,7 @@ function withoutConsumablesNull(arr) {
   return newDatas;
 }
 
+//nullokból unknown
 function everyWillBeUnknown(arr) {
   for (var i = 0; i < arr.length; i++) {
     for (var j = 0; j < arr[i].length; j++)
@@ -69,6 +70,7 @@ function everyWillBeUnknown(arr) {
   return arr;
 }
 
+//kártyák létrehozása
 var objShapceshipList = document.querySelector(".shapceship-list")
 
 function cartes(person, containerPanels) {
@@ -76,8 +78,10 @@ function cartes(person, containerPanels) {
   var objImgDiv = document.createElement('DIV');
   var objImg = document.createElement('IMG');
   var objDatasDiv = document.createElement('DIV');
+  var objStatisticsDiv = document.createElement('DIV');
 
   objShapceshipList.appendChild(objCartes);
+  objShapceshipList.appendChild(objStatisticsDiv);
   objCartes.classList.add('cartes');
   objCartes.appendChild(objImgDiv);
   objImgDiv.classList.add('imgdiv');
@@ -87,6 +91,54 @@ function cartes(person, containerPanels) {
   objDatasDiv.classList.add('datasdiv');
   objDatasDiv.innerHTML = `${person.consumables}<br>${person.denomination}<br>${person.cargo_capacity}<br>${person.passengers}<br>${person.max_atmosphering_speed}<br>${person.crew}<br>${person.lengthiness}<br>${person.model}<br>${person.cost_in_credits}<br>${person.manufacturer}`
 }
+/*
+ 
+ 
+ * A leghosszabb(lengthiness) hajó képe*/
+
+//egy fős legénységűek
+function oneCrew(datasArray) {
+  var piece = 0;
+  for (var i = 0; i < datasArray.length; i++) {
+    if (datasArray[i].crew === 1) {
+      piece += 1;
+    }
+  }
+  return `Az egy fős legénységgel rendelkező hajók darabszáma: ${piece}`;
+}
+
+//legnagyobb cargo_capacity
+function maxCargo(datasArray) {
+  var maxcargoname = datasArray[0];
+  for (var i = 0; i < datasArray.length; i++) {
+    if (datasArray[i].cargo_capacity > maxcargoname.cargo_capacity) {
+      maxcargoname = datasArray[i];
+    }
+  }
+  return maxcargoname.model;
+}
+
+//Az össze hajó összes utasa
+function sumPassengers(datasArray) {
+  var allpassenger = 0;
+  for (var i = 0; i < datasArray.length; i++) {
+    allpassenger += datasArray[i].passengers;
+  }
+  return allpassenger;
+}
+
+//leghoszabb hajó képe
+function pictureMaxLength(datasArray) {
+  var maxlength = datasArray[0];
+  for (var i = 0; i < datasArray.length; i++) {
+    if (datasArray[i].lengthiness > maxlength.lengthiness) {
+      maxlength = datasArray[i];
+    }
+  }
+  return maxlength.image
+}
+
+
 
 function getCartes(array, place) {
 
