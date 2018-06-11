b8f92abac68d86a82163f84433b6f9dc63e76c13
Gyarmathy Eszter <tucs2010@gmail.com>
2018-06-11 13:28:50 +0200

    costCreditAsc

--- js/app.js
+++ js/app.js
@@ -12,8 +12,28 @@ function getData(url, callbackFunc) {
 function successAjax(xhttp) {
   // Innen lesz elérhető a JSON file tartalma, tehát az adatok amikkel dolgoznod kell
   var userDatas = JSON.parse(xhttp.responseText);
-  console.log(userDatas);
-
+  console.log(costCreditsAsc(userDatas));
 }
 
-getData('/json/spaceships.json', successAjax);
+function costCreditsAsc(arr) {
+  var i = arr.length;
+  var swap = false;
+
+  do {
+    swap = false;
+    for (var j = 0; j < i - 1; j++) {
+      if (arr[j].cost_in_credits === null) {
+        arr[j].cost_in_credits = 0;
+      };
+      if (parseInt(arr[j].cost_in_credits) > parseInt(arr[j + 1].cost_in_credits)) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        swap = true;
+      }
+    }
+    i--;
+  } while (i >= 0 && swap);
+  return arr;
+}
+
+
+getData("/json/spaceships.json", successAjax);
